4ff09818f97737eb80117524bd9bc8a63b3da38c
Gabriel Rocha <rochagbriel@gmail.com>
2023-11-14 13:19:46 +0100

    5.11

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,18 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
-    end
-    
-    def show
-        @article = Article.find(params[:id])
-    end
+  def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
+  end
     
-def new
+  def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
    
   def create
@@ -18,10 +21,20 @@ def new
     if @article.save
       redirect_to @article
     else
-        render 'new'
+      render 'new'
     end
   end
-   
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
